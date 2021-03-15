2a49ea076d4b1adb53c7c554446e39464e5e4ec9
EntingChou <andyjf2213@gmail.com>
2021-03-15 16:32:23 +0800

    Add files via upload

---
+++ plot_bounding_box_script_by_Enting.py
@@ -0,0 +1,35 @@
+import json 
+import cv2
+ 
+# Opening JSON file 
+# f = open('/home/enting/桌面/點膠&缺件/Testing_20210312/nnnn-3.json',) 
+print("Your inferred JSON path")
+f = open(input())
+
+print('Your inferred images path')
+image_path = input()
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+   
+# Iterating through the json 
+# list 
+x = int(0)
+for i in data['annotations']: 
+    image_id = i['image_id']
+    score = i['score']
+    category_id = i['category_id']
+    x, y, w, h  = i['bbox']       
+    
+    fname = image_path+'/'+str(image_id)+'.png'
+    img = cv2.imread(fname)
+    
+    cv2.rectangle(img, (int(x),int(y)), (int(x)+int(w),int(y)+int(h)), (0,255,0), 4)
+    # 标注文本
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = str(score)+'|'+str(category_id)
+    cv2.putText(img, text, (212, 310), font, 1, (0,255,255), 1, cv2.LINE_AA)
+
+    cv2.imwrite('pic_'+str(x)+'.jpg', img)
+    x = x + 1
+f.close() 
